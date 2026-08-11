41ccb063af90b136e61747f6f43631ff1b1cfdfd
Ricardo Pereira da Silva <ricardo9pereira@hotmail.com>
2026-08-11 11:39:09 -0300

    Criada_a_controller_Aula2

---
+++ src/main/java/com/ripsxt/cadastro/Controller.java
@@ -0,0 +1,15 @@
+package com.ripsxt.cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Primeira mensagem nesta rota";
+    }
+}
